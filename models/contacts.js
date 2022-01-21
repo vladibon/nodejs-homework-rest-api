632c7b3333a80e122b0f2525217204fa78a4af1d
Vladimir Bondar <vladi.bondar@gmail.com>
2022-01-22 01:28:13 +0200

    delete extra comments

--- models/contacts.js
+++ models/contacts.js
@@ -1,17 +1,3 @@
-// const listContacts = async () => {};
-// const getContactById = async contactId => {};
-// const removeContact = async contactId => {};
-// const addContact = async body => {};
-// const updateContact = async (contactId, body) => {};
-
-// module.exports = {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-//   updateContact,
-// };
-
 const fs = require('fs/promises');
 const { nanoid } = require('nanoid');
 
